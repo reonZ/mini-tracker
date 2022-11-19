70f3355634107644d715c0c29f976e958f22e3e6
Idle <651462+reonZ@users.noreply.github.com>
2022-11-19 23:58:18 +0100

    make sure the right pf2e metagame is returned

--- scripts/combatant.js
+++ scripts/combatant.js
@@ -7,6 +7,9 @@ export function playersCanSeeCombatantName(combatant) {
 }
 
 export function hideCreaturesName() {
-    if (game.system.id === 'pf2e') return !!game.settings.get('pf2e', 'metagame_tokenSetsNameVisibility')
-    else return !!getSetting('creature')
+    if (game.system.id === 'pf2e') {
+        if (game.settings.settings.has('pf2e.metagame.tokenSetsNameVisibility'))
+            return !!game.settings.get('pf2e', 'metagame.tokenSetsNameVisibility')
+        else return !!game.settings.get('pf2e', 'metagame_tokenSetsNameVisibility')
+    } else return !!getSetting('creature')
 }
